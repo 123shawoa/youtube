2297c25d68388ae8ce9d188cd4b98297eb328a38
Daniel <139485889+123shawoa@users.noreply.github.com>
2025-01-26 20:39:34 -0500

    cool

---
+++ youtube.py
@@ -0,0 +1,12 @@
+import tkinter
+import customtkinter
+from pytube import YouTube
+
+
+# System setting
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
+
+#Our app frame
+app = customtkinter.CTk()
+app.geometry("720x480")
